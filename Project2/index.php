5dc89e5ec771ba3887dadcfdf39e99decae050b9
muthusiva12 <muthusiva23092004@gmail.com>
2024-05-30 19:45:38 +0530

    Add files via upload

---
+++ Project2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="containers">
+        <h1>Successfully Login</h1>
+        <a href="logout.php" class="btn btn-warning">Logout</a>
+        <a href="Mainpage1.php" class="btn btn-warning">Return Home</a>
+    </div>
+</body>
+
+</html>
